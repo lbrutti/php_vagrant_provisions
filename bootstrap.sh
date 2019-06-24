cd72a027fb7028af255d65c51091e57fdb871893
lorenzo.brutti <lorenzo.brutti@metide.com>
2019-06-24 15:46:58 +0200

    non interactive mariadb installation

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,7 +9,12 @@ apt-get update
 apt-get install -y software-properties-common
 apt-get install -y php7.2
 apt-get install -y php-pear php7.2-curl php7.2-dev php7.2-gd php7.2-mbstring php7.2-zip php7.2-mysql php7.2-xml
+#install Maria db non interactively
+export DEBIAN_FRONTEND="noninteractive"
+debconf-set-selections <<< "mariadb-server mysql-server/root_password password $PASSWORD"
+debconf-set-selections <<< "mariadb-server mysql-server/root_password_again password $PASSWORD" 
 apt-get install -y mariadb-server
+
 apt-get install -y apache2
 
 if ! [ -L /var/www ]; then
